02c3a195a4194f245b5bbee9f7ef213edb61d149
Herman <herman_cheah@gamlite.com>
2022-06-27 12:24:48 +0800

    initial

---
+++ Startup.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Security;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Logging;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ClientApi
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ILogger _logger;
+        public Startup(IConfiguration configuration, IWebHostEnvironment webHostEnvironment)
+        {
+
+            this._configuration = configuration;
+            //this._httpContextAccessor = httpContextAccessor;
+            this._webHostEnvironment = webHostEnvironment;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var identityUrl = this._configuration.GetValue<string>("IdentityUrl");
+            var idscope = this._configuration.GetValue<string>("IdentityScope");
+            services.AddCors();
+
+            // adds DI services to DI and configures bearer as the default scheme
+            services.AddAuthentication("Bearer")
+                .AddJwtBearer("Bearer", options =>
+                {
+                    // identity server issuing token
+                    options.Authority = identityUrl;
+                    options.RequireHttpsMetadata = false;
+
+                    // allow self-signed SSL certs
+                    //options.BackchannelHttpHandler = new HttpClientHandler { ServerCertificateCustomValidationCallback = delegate { return true; } };
+
+                    // the scope id of this api
+                    options.Audience = idscope;
+                });
+
+            services.AddMvcCore(options => {
+                options.EnableEndpointRouting = false;
+            })
+                .AddAuthorization();
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            IdentityModelEventSource.ShowPII = true;
+
+            app.UseCors(builder =>
+                 builder
+                   .WithOrigins("http://localhost:8080", "https://localhost:44302", "http://localhost:8802")
+                   .AllowAnyHeader()
+                   .AllowAnyMethod()
+                   .AllowCredentials()
+               );
+            // adds authentication middleware to the pipeline so authentication will be performed on every request
+            app.UseAuthentication();
+            app.UseMvc();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Doughnut API is running!");
+            });
+        }
+    }
+}
