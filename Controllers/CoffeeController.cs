8658e93c8698e35c67a7a0b17fadabd84d736de6
Herman <herman_cheah@gamlite.com>
2022-06-27 12:27:29 +0800

    locals

---
+++ Controllers/CoffeeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("coffee")]
+    [ApiController]
+    [AllowAnonymous]
+    public class CoffeeController : ControllerBase
+    {
+        private readonly List<Coffee> coffees = new List<Coffee>()
+        {
+            new Coffee
+            {
+                Name = "Americano", 
+                Price = 1.99
+            },
+            new Coffee
+            {
+                Name = "Latte", 
+                Price = 2.99
+            },
+            new Coffee
+            {
+                Name = "Espresso", 
+                Price = 2.99
+            },
+        };
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(coffees);
+        }
+    }
+}
---
+++ Models/Doughnut.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace WebAPI.Models
+{
+    public class Doughnut
+    {
+        public string Name { get; set; }
+        public string Filling { get; set; }
+        public bool Iced { get; set; }
+        public double Price { get; set; }
+    }
+    public class Coffee
+    {
+        public string Name { get; set; } 
+        public double Price { get; set; }
+    }
+}
